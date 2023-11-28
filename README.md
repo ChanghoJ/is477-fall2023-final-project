b844311b60aab18f0207b086ab205dfa06038ca1
ChanghoJ <chjung2@illinois.edu>
2023-11-28 12:29:28 -0600

    prepare data script added

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # is477-fall2023-final-project
 Public repository for IS477 Fall 2023 final project
+
+## Overview
+
+## Contributions
+
+## Analysis
+
+## Workflow
+
+## Reproducing
+
+## License
+- Dataset: 
+
+## References
+- Aeberhard,Stefan and Forina,M.. (1991). Wine. UCI Machine Learning Repository. https://doi.org/10.24432/C5PC7J.
---
+++ scripts/prepare_data.py
@@ -0,0 +1,38 @@
+# 1 hash key and file name
+HASH_WINE = "2bae62c4481220623579d4c4fb36b55652b6b75e06e49fa1981b8198362dfdab"
+FILE_WINE = "data/wine.zip"
+
+# 2 downloads and verifies both datasets
+import requests
+import os
+import hashlib
+
+##import zipfile
+
+# UCI Wine dataset
+url01 = 'https://archive.ics.uci.edu/static/public/109/wine.zip'
+
+# get response from the download
+try:
+    response01 = requests.get(url01)
+except:
+    print("failed to connect wine.zip. check url")
+
+# download if data/wine.zip not exist
+if os.path.exists(FILE_WINE) is False:
+    try:
+        with open(FILE_WINE, mode='wb') as f:
+            f.write(response01.content)
+    except:
+        print("failed to download wine.zip")
+
+# get hash from the data
+with open(FILE_WINE, 'rb') as f:
+    data01 = f.read()
+    sha256hash01 = hashlib.sha256(data01).hexdigest()
+
+# comparing hash and send error messege
+if HASH_WINE != sha256hash01:
+    print("wine.zip hash does not match expected hash, not matching with original file") 
+else:
+    print("wine.zip hash matches expected hash")
